cb31bc045e63934a1bb877f4d83f60954d3dabba
Alexander Kazakov <small202020@gmail.com>
2022-06-24 17:02:05 +0700

    up

--- server.js
+++ server.js
@@ -7,12 +7,7 @@ Promise.config({
     cancellation: true
 });
 const mysql = require('mysql');
-// const connection = mysql.createConnection({
-//     host     : process.env.DB_HOST,
-//     database : process.env.DB_NAME,
-//     user     : process.env.DB_USER,
-//     password : process.env.DB_PASSWORD
-// });
+
 
 var db_config = {
     host     : process.env.DB_HOST,
@@ -24,22 +19,19 @@ var db_config = {
 var connection;
 
 function handleDisconnect() {
-    connection = mysql.createConnection(db_config); // Recreate the connection, since
-                                                    // the old one cannot be reused.
-
-    connection.connect(function(err) {              // The server is either down
-        if(err) {                                     // or restarting (takes a while sometimes).
+    connection = mysql.createConnection(db_config);
+    connection.connect(function(err) {
+        if(err) {
             console.log('error when connecting to db:', err);
-            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-        }                                     // to avoid a hot loop, and to allow our node script to
-    });                                     // process asynchronous requests in the meantime.
-                                            // If you're also serving http, display a 503 error.
+            setTimeout(handleDisconnect, 2000);
+        }
+    });
     connection.on('error', function(err) {
         console.log('db error', err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-            handleDisconnect();                         // lost due to either server restart, or a
-        } else {                                      // connnection idle timeout (the wait_timeout
-            throw err;                                  // server variable configures this)
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();
+        } else {
+            throw err;
         }
     });
 }
